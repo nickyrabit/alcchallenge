1e7ea9d370a8a1c1388e2bd4ced390f19ddec59d
Nicky Ngailo <nickyrabit@gmail.com>
2019-07-15 15:01:19 +0300

    ui design on the c activity and b

--- app/src/main/java/com/runner/alc/ActivityB.java
+++ app/src/main/java/com/runner/alc/ActivityB.java
@@ -3,11 +3,15 @@ package com.runner.alc;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 
 public class ActivityB  extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
+
+        WebView mywebview = findViewById(R.id.aboutWebView);
+        mywebview.loadUrl("https://andela.com/alc/");
     }}
 
