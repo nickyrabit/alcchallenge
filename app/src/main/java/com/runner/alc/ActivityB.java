cf66b27f9f75518f9c19b7aacc389ac6d3d18399
Nicky Ngailo <nickyrabit@gmail.com>
2019-07-15 15:43:05 +0300

    handling ssl form https

--- app/src/main/java/com/runner/alc/ActivityB.java
+++ app/src/main/java/com/runner/alc/ActivityB.java
@@ -1,9 +1,13 @@
 package com.runner.alc;
 
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class ActivityB  extends AppCompatActivity {
     @Override
@@ -12,6 +16,24 @@ public class ActivityB  extends AppCompatActivity {
         setContentView(R.layout.activity_b);
 
         WebView mywebview = findViewById(R.id.aboutWebView);
-        mywebview.loadUrl("https://andela.com/alc/");
-    }}
+
+        mywebview.setWebViewClient(new WebViewClient());
+        WebSettings settings = mywebview.getSettings();
+        settings.setSupportMultipleWindows(true);
+        settings.getJavaScriptEnabled();
+
+        mywebview.getSettings().setJavaScriptEnabled(true);
+        mywebview.getSettings().setSupportZoom(true);
+
+mywebview.setWebViewClient(new WebViewClient(){
+    @Override
+    public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+        handler.proceed();
+    }
+});
+        mywebview.loadUrl("https://andela.com/alc");
+    }
+
+
+}
 
