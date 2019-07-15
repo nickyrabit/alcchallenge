79020895171971ce9ad3664c3b4835edb85606ab
Nicky Ngailo <nickyrabit@gmail.com>
2019-07-15 14:54:12 +0300

    setting buttons on th emani activity

--- app/src/main/java/com/runner/alc/MainActivity.java
+++ app/src/main/java/com/runner/alc/MainActivity.java
@@ -1,13 +1,39 @@
 package com.runner.alc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+Button atoB_Button, btoC_Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        atoB_Button = findViewById(R.id.buttonToActivityB);
+        btoC_Button = findViewById(R.id.buttonToActivityC);
+
+
+
+        atoB_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+
+        btoC_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
